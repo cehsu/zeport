4ff04320ac1257d055b2f3d7c6c4158ac5bac3d7
Claire Hsu <clairehsu86@gmail.com>
2017-01-04 11:07:47 -0800

    animate header shrink on scroll

--- src/javascripts/header.js
+++ src/javascripts/header.js
@@ -4,12 +4,28 @@ import 'stylesheets/modules/header'
 import 'stylesheets/utilities/clearfix'
 
 const Header = React.createClass({
+  getInitialState: function() {
+    return {shrink: false}
+  },
   render () {
     return (
-      <div className='header u-clearfix'>
-        Header
+      <div className={this.state.shrink ? 'shrink header u-clearfix' : 'header u-clearfix'}>
+        <div className={this.state.shrink ? 'smaller header-content u-clearfix': 'header-content u-clearfix'}>Header</div>
       </div>
     )
+  },
+  componentDidMount: function() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  componentWillUnmount: function() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  handleScroll: function(event) {
+    if(window.document.body.scrollTop>100){
+      this.setState({shrink: true});
+    } else {
+      this.setState({shrink: false});
+    }
   }
 })
 
