89236fe776fe42279376945e0cff9bd4c0300935
Claire Hsu <clairehsu86@gmail.com>
2017-01-11 15:21:20 -0800

    update build

--- src/javascripts/images.js
+++ src/javascripts/images.js
@@ -27,7 +27,6 @@ var Images = [
   "url": "http://zesansan.com/images/work/comics/thumbs/50keepfresh_thumbs.jpg",
   "slideshow": [
     "http://zesansan.com/images/work/comics/thumbs/50keepfresh_thumbs.jpg",
-    "http://zesansan.com/images/work/comics/thumbs/45pcc_thumbs.jpg",
     "http://zesansan.com/images/work/comics/thumbs/47jianbing_thumbs.jpg",
     "http://zesansan.com/images/work/comics/thumbs/46antiques_thumbs.jpg",
     "http://zesansan.com/images/work/comics/thumbs/50keepfresh_thumbs.jpg",
