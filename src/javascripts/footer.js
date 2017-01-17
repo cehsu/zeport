ed1e26b123a79730359278069d5e567683bde76c
Claire Hsu <clairehsu86@gmail.com>
2017-01-16 16:23:07 -0800

    compose email on click

--- src/javascripts/footer.js
+++ src/javascripts/footer.js
@@ -1,13 +1,15 @@
 import React from 'react'
-
-import 'stylesheets/modules/footer'
 import 'stylesheets/utilities/_reset'
+import 'stylesheets/modules/footer'
+
 
 const Footer = React.createClass({
   render () {
     return (
       <div className={'footer'}>
+        <a href='mailto:zesansan@gmail.com?Subject=Hello%20Zejian'>
         zesansan@gmail.com
+        </a>
       </div>
     )
   }
