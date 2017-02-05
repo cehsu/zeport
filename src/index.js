b11260fdbcd2c8a318af33278bbbaf4da7783c31
Claire Hsu <clairehsu86@gmail.com>
2017-02-05 13:52:27 -0800

    smooth scroll to top

--- src/index.js
+++ src/index.js
@@ -9,34 +9,68 @@ class App extends React.Component {
   render() {
     return (
         <Router  history={ hashHistory }>
-          <Route 
-          onChange={(prevState, nextState) => {
-            var w = window.innerWidth
-              || document.documentElement.clientWidth
-              || document.body.clientWidth;
-            if(w < 700){
-              if(nextState.params.piece) {
-                window.scroll(0, 60);
-              }
-              if(nextState.params.route === "about") {
-                window.scroll(0,5);
-              }
-            } else {
-              if(nextState.params.piece) {
-                window.scroll(0, 330);
-              }
-              if(nextState.params.route === "about") {
-                window.scroll(0,250);
-              }
+        <Route 
+        onChange={(prevState, nextState) => {
+          var w = window.innerWidth
+            || document.documentElement.clientWidth
+            || document.body.clientWidth;
+          var h = window.scrollY;
+          if(w < 700){
+            if(nextState.params.piece) {
+              var interval = ((h -60)/ 10);
+              var smooth = setInterval(function(){
+                if(h>61) {
+                  h-=interval
+                    window.scroll(0, h);
+                } else {
+                  clearInterval(smooth);
+                }
+              }, 25);
             }
-          }}
-          path='/(:route(/:piece(/:number)))' 
+            if(nextState.params.route === "about") {
+              var interval = ((h-5)/10);
+              var smooth = setInterval(function(){
+                if(h>6) {
+                  h-=interval;
+                  window.scroll(0, h);
+                } else {
+                  clearInterval(smooth);
+                }
+              }, 25);
+            }
+          } else {
+            if(nextState.params.piece) {
+              var interval = ((h - 330)/10);
+              var smooth = setInterval(function(){
+                if(h > 331) {
+                  h-=interval;
+                  window.scroll(0, h);
+                } else {
+                  clearInterval(smooth);
+                }
+              }, 25);
+            }
+            if(nextState.params.route === "about") {
+              var interval = ((h-250)/10);
+              var smooth = setInterval(function(){
+                if(h >251) {
+                  h-=interval;
+                  window.scroll(0,h);
+                } else {
+                  clearInterval(smooth);
+                }
+              }, 25);
+            }
+          }
+        }
+        }
+        path='/(:route(/:piece(/:number)))' 
           component={Container}
-          >
+        >
           </Route>
           </Router>
-        );
+          );
+        }
   }
-}
 
-ReactDOM.render(<App />, document.querySelector('#main'))
+  ReactDOM.render(<App />, document.querySelector('#main'))
