5e55b75226577adca840a7a21f033076cdeefa75
Claire Hsu <clairehsu86@gmail.com>
2017-02-11 18:22:54 -0800

    make showcase-functions less verbose

--- src/javascripts/showcase-functions.js
+++ src/javascripts/showcase-functions.js
@@ -4,41 +4,26 @@ export function setItem(width, pieceIndex, dimensions) {
   const mobile = !(width > 700);
   const showcaseWidth = (width * 0.9);
   const showcaseHeight = 600;
-  const limitDiff = (showcaseWidth - showcaseHeight);
   if(dimensions){
-    console.log('it begins');
-    console.log(dimensions[pieceIndex]);
-    console.log(mobile, 'mobile');
     numHeight = dimensions[pieceIndex][1].replace(/[px]/gi, '');
     numWidth = dimensions[pieceIndex][0].replace(/[px]/gi, '');
-    console.log(numWidth - showcaseWidth, 'widthdiff');
-    console.log(numHeight - showcaseHeight, 'heightdiff');
     //resize wide items and mobile items to showcase width
     if (mobile){
-      console.log('in mobile');
       itemWidth = "100%";
       itemHeight = "auto";
     } else {
-      console.log('in not mobile');
-      if ((numWidth -( showcaseWidth + limitDiff )) > (numHeight - showcaseHeight)){
-         console.log('toowide', numWidth, numHeight); 
+      if ((numWidth - showcaseWidth) > (numHeight - showcaseHeight)){
          numHeight = (showcaseWidth/numWidth)*numHeight;
          numWidth = showcaseWidth;
-         console.log(numWidth, numHeight);  
     //resize tall items to showcase height
     } if(numHeight > showcaseHeight) {
-      console.log('in too tall');
-      console.log(numWidth, numHeight); 
       numWidth = (600 / numHeight ) * numWidth;
       numHeight = showcaseHeight;
     //format already appropriately dimensioned items
     }
-      console.log('format non mobile');
       itemHeight = numHeight+"px";
       itemWidth = numWidth + "px";
     }
   }   
-    console.log('it ends');
-    console.log(itemWidth, itemHeight);
     return {itemHeight: itemHeight, itemWidth: itemWidth};
 }
