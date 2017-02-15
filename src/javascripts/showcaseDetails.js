634afdf066c94547d064bf133920ac996840bdd5
Claire Hsu <clairehsu86@gmail.com>
2017-02-14 16:20:37 -0800

    address style nits

--- src/javascripts/showcaseDetails.js
+++ src/javascripts/showcaseDetails.js
@@ -13,15 +13,15 @@ class ShowcaseDetails extends React.Component {
       const landscape = (((showcaseItem.type === 'Animation') || (showcaseItem.type === 'Film'))&&(slideshow[0].indexOf('gif')===-1));
       const {name, year, materials, team, description, client, clients, press} = showcaseItem;
       const allProperties = [];
-      allProperties.push(name, year, materials, team, description, client, clients);
-      const allDetails = allProperties.filter(function(item, index){ return item;});
+      allProperties.push(["name", name], ["year", year], ["materials", materials], ["team", team], ["client", client], ["description", description],["clients", clients]);
+      const allDetails = allProperties.filter(function(item, index){ return item[1];});
       return (
           <div className={ (landscape) ? 'item-details landscape' : 'item-details'}> 
             {allDetails.map(function(detail, index){
-              return <div key={index}>{detail}</div>
+              return <div className={detail[0]} key={index}>{(detail[0] === "client") ? <span><strong>"Client: "</strong>{detail[1]}</span> : (detail[0] === "clients") ? <span><strong>Select Clients: </strong>{detail[1]}</span> : detail[1]}</div>
               })
             }
-            {press && <div>Press: {showcaseItem.press.map(function(item, index, collection){
+            {press && <div className={"press"}><strong>Press: </strong>{showcaseItem.press.map(function(item, index, collection){
               if(index < (collection.length - 1)){
                 return <span key={index}><a href={item[1]}>{item[0]}</a>, </span>
               } else {
